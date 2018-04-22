c791b3153fbd3e6d3e546f9dcfa5bbebf3a3ed76
node-toso-api <alisonsmithaccy@gmail.com>
2018-04-22 17:17:41 +0100

    added PATCH method

--- server/server.js
+++ server/server.js
@@ -1,76 +1,109 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const {ObjectID} = require('mongodb')
-const {mongoose} = require('./db/mongoose')
-const {Todo} = require('./models/todo')
-const {User} = require('./models/user')
-const port = process.env.PORT || 3000
+const express = require("express")
+const _ = require("lodash")
+const bodyParser = require("body-parser")
+
+const { ObjectID } = require("mongodb")
+const { mongoose } = require("./db/mongoose")
+const { Todo } = require("./models/todo")
+const { User } = require("./models/user")
 
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
-app.post('/todos', (req, res) => {
+// PUSH a new todo to DB
+app.post("/todos", (req, res) => {
   var todo = new Todo({
     text: req.body.text
   })
-  todo.save()
-  .then((doc) => {
-    res.send(doc)
-  })
-  .catch ((e) => {
-    res.status(400).send(e)
-  })
-})
-
-app.get('/todos', (req, res) => {
-  Todo.find()
-    .then((todos) => {
-      res.send({todos})
+  todo
+    .save()
+    .then(doc => {
+      res.send(doc)
     })
-    .catch ((e) => {
+    .catch(e => {
       res.status(400).send(e)
     })
 })
 
-app.get('/todos/:id', (req, res) => {
+//GET All todos from DB
+app.get("/todos", (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({ todos })
+    })
+    .catch(e => {
+      res.status(400).send(e)
+    })
+})
+
+//GET A todos from DB
+app.get("/todos/:id", (req, res) => {
   var id = req.params.id
   if (!ObjectID.isValid(id)) {
     return res.status(404).send()
   }
 
   Todo.findById(id)
-    .then((todo) => {
+    .then(todo => {
       if (!todo) {
         return res.status(404).send()
       }
-      res.send({todo})
+      res.send({ todo })
     })
-    .catch((e) => {
+    .catch(e => {
       res.status(400).send()
     })
 })
 
-app.delete('/todos/:id', (req, res) => {
+//DELETE a todos from DB
+app.delete("/todos/:id", (req, res) => {
   var id = req.params.id
   if (!ObjectID.isValid(id)) {
     return res.status(404).send()
   }
-   
+
   Todo.findByIdAndRemove(id)
-    .then((todo) => {
-      if (!todo)  {
-        return res.status(404).send({message: "Item could not be found"})
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send({ message: "Item could not be found" })
       }
-      res.status(200).send({todo: todo})
+      res.status(200).send({ todo: todo })
     })
-    .catch ((e) => {
+    .catch(e => {
       res.status(400).send(e)
     })
+})
+
+app.patch("/todos/:id", (req, res) => {
+  const id = req.params.id
+  const body = _.pick(req.body, ["text", "completed"])
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then(todo => {
+      if (!todo) {
+        return res.status(400).send()
+      }
+      res.status(200).send({todo})
+    })
+    .catch(e => {
+      res.status(400).send(e)
+  })
 })
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`)
 })
 
-module.exports = {app}
+module.exports = { app }
