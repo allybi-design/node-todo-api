5856f6cbffc3fa0852967a6e7c0134f4fa9b1ad5
node-toso-api <alisonsmithaccy@gmail.com>
2018-06-02 13:28:38 +0100

    update config to use JSON file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -27,7 +27,7 @@ const users = [
     password: 'mysecret',
     tokens: [{
       access: 'auth',
-      token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+      token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString()
     }]
   },
   {
@@ -36,7 +36,7 @@ const users = [
     password: 'abc123',
     tokens: [{
       access: 'auth',
-      token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+      token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString()
     }]
   }
 ]
